eb2b23f717b9c6cfbcfcbf3bb683b6699f1d8390
chaelink <lin1041840@gmail.com>
2024-04-19 04:18:34 +0900

    MyEat Project Init

--- src/main/java/MyEat/myeat/MyeatApplication.java
+++ src/main/java/MyEat/myeat/MyeatApplication.java
@@ -1,7 +1,5 @@
 package MyEat.myeat;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
