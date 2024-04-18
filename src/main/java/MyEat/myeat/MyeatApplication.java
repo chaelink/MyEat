ed04919d3a3d63afdbf88bce49bacebd2ab2feff
chaelink <lin1041840@gmail.com>
2024-04-19 04:17:55 +0900

    MyEat Project Init

---
+++ src/main/java/MyEat/myeat/MyeatApplication.java
@@ -0,0 +1,16 @@
+package MyEat.myeat;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MyEat/myeat/MyeatApplicationTests.java
@@ -0,0 +1,13 @@
+package MyEat.myeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
