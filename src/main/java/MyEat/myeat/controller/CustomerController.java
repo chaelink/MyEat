d3268663c5fe8332e02fdb53fad8114feb04af52
chaelink <lin1041840@gmail.com>
2024-04-25 19:27:29 +0900

    Customer Controller update

--- src/main/java/MyEat/myeat/controller/CustomerController.java
+++ src/main/java/MyEat/myeat/controller/CustomerController.java
@@ -2,6 +2,7 @@ package MyEat.myeat.controller;
 
 import MyEat.myeat.domain.Customer;
 import MyEat.myeat.service.CustomerService;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -13,19 +14,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Tag(name="Customer API", description = "개인 고객 API")
 @RestController
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
 
+    @Hidden
     @GetMapping(value = "/members/new")
     public String createForm(Model model) {
         model.addAttribute("CustomerForm", new CustomerForm());
         return "members/createCustomerForm";
     }
 
-    @Operation(summary = "고객 회원가입 폼")
+    @Operation(summary = "Customer 등록")
     @PostMapping(value = "/members/new")
     public String create(@Valid CustomerForm customerForm, BindingResult result) {
         if (result.hasErrors()) {
@@ -41,5 +45,13 @@ public class CustomerController {
 
         customerService.join(customer);
         return "redirect:/";
+    }
+
+    @Operation(summary = "Customer 목록 조회")
+    @GetMapping(value= "/members")
+    public String list(Model model) {
+        List<Customer> customers = customerService.findAll();
+        model.addAttribute("customers", customers);
+        return "members/customerList";
     }
 }
