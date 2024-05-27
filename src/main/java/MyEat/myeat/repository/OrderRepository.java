316b32e5ec40b222170352d1b998ef027f67f233
chaelink <lin1041840@gmail.com>
2024-05-27 16:56:43 +0900

    OrderRepository ADD

--- src/main/java/MyEat/myeat/repository/OrderRepository.java
+++ src/main/java/MyEat/myeat/repository/OrderRepository.java
@@ -1,4 +1,19 @@
 package MyEat.myeat.repository;
 
+import MyEat.myeat.domain.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 public class OrderRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findByLoginId(String loginId) {
+        return em.find(Order.class, loginId);
+    }
 }
