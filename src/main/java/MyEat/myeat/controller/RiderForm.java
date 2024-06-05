905efb57824df425e7f241e123a2e8e9bc044d58
chaelink <lin1041840@gmail.com>
2024-06-05 23:19:02 +0900

    RiderForm add

---
+++ src/main/java/MyEat/myeat/controller/RiderForm.java
@@ -0,0 +1,13 @@
+package MyEat.myeat.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RiderForm {
+
+    private String loginId;
+    private String password;
+    private String name;
+    private String phoneNumber;
+}
