b17c98f5be17a7b3709f8eead85941c1c4473484
chaelink <lin1041840@gmail.com>
2024-06-06 20:32:28 +0900

    Controller edit

--- src/main/java/MyEat/myeat/controller/CustomerController.java
+++ src/main/java/MyEat/myeat/controller/CustomerController.java
@@ -2,7 +2,6 @@ package MyEat.myeat.controller;
 
 import MyEat.myeat.domain.Customer;
 import MyEat.myeat.service.CustomerService;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -12,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,18 +22,18 @@ public class CustomerController {
     private final CustomerService customerService;
 
     //@Hidden
-    @GetMapping(value = "/members/new")
+    @GetMapping(value = "/customers/new")
     public String createForm(Model model) {
         model.addAttribute("CustomerForm", new CustomerForm());
-        return "members/createCustomerForm";
+        return "customers/createCustomerForm";
     }
 
 
     @Operation(summary = "Customer 등록")
-    @PostMapping(value = "/members/new")
+    @PostMapping(value = "/customers/new")
     public String create(@Valid CustomerForm customerForm, BindingResult result) {
         if (result.hasErrors()) {
-            return "members/createCustomerForm";
+            return "customers/createCustomerForm";
         }
 
         Customer customer = new Customer();
@@ -52,10 +50,10 @@ public class CustomerController {
 
 
     @Operation(summary = "Customer 목록 조회")
-    @GetMapping(value= "/members")
+    @GetMapping(value= "/customers")
     public String list(Model model) {
         List<Customer> customers = customerService.findAll();
         model.addAttribute("customers", customers);
-        return "members/customerList";
+        return "customers/customerList";
     }
 }
--- src/main/java/MyEat/myeat/controller/RiderController.java
+++ src/main/java/MyEat/myeat/controller/RiderController.java
@@ -1,9 +1,12 @@
 package MyEat.myeat.controller;
 
+import MyEat.myeat.domain.Rider;
 import MyEat.myeat.service.RiderService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Tag(name="Rider API", description = "라이더 API")
 @Controller
@@ -11,6 +14,12 @@ import org.springframework.stereotype.Controller;
 public class RiderController {
 
     private final RiderService riderService;
+
+    @GetMapping(value = "/riders/new")
+    public String createForm(Model model) {
+        model.addAttribute("RiderForm", new RiderForm());
+        return "riders/createRiderForm";
+    }
 
 
 }
