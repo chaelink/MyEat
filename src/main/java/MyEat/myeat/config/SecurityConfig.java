b741b1dfd03092dba0f9b021ae6371ef68ae1e6f
chaelink <lin1041840@gmail.com>
2024-06-13 23:10:15 +0900

    SpringSecurity Init

---
+++ src/main/java/MyEat/myeat/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package MyEat.myeat.config;
+
+
+import MyEat.myeat.service.CustomUserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+
+                )
+                .formLogin(form -> form
+                        .loginPage("/customers/login")
+                        .permitAll()
+
+                )
+                .logout(logout -> logout.permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailService();
+    }
+}
---
+++ src/main/java/MyEat/myeat/service/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package MyEat.myeat.service;
+
+import MyEat.myeat.domain.Customer;
+import MyEat.myeat.repository.CustomerRepository;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByLoginId(loginId);
+        if (customer == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(customer.getLoginId(), customer.getPassword(), new ArrayList<>());
+    }
+}
