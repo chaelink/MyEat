b6a7c84c26efab2370793baaee106019b854af7d
chaelink <lin1041840@gmail.com>
2024-09-16 19:42:19 +0900

    Order 리팩토링

--- src/main/java/MyEat/myeat/controller/OrderController.java
+++ src/main/java/MyEat/myeat/controller/OrderController.java
@@ -10,19 +10,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
 
-    private final CustomerService customerService;
     private final RestaurantService restaurantService;
     private final OrderService orderService;
-    private final MenuService menuService;
 
     @PostMapping(value = "/order")
     public String order(HttpSession session, @RequestParam("restaurantId") Long restaurantId,
@@ -35,32 +30,7 @@ public class OrderController {
         Customer customer = (Customer) session.getAttribute("customerLoggedIn");
         Restaurant restaurant = restaurantService.findById(restaurantId);
 
-        Map<Long, OrderItem> orderMenus = new HashMap<>();
-        Long totalPrice = 0L;
-
-        for (Map.Entry<String, String> entry : menuQuantities.entrySet()) {
-            if (entry.getKey().startsWith("menuQuantities[")) {
-                Long menuId = Long.parseLong(entry.getKey().substring(15, entry.getKey().length() - 1));
-                int quantity = Integer.parseInt(entry.getValue());
-
-                if (quantity > 0) {
-                    Menu menu = menuService.findById(menuId); // 메뉴 정보를 DB에서 가져옴
-                    Long menuPrice = menu.getMenuPrice();
-                    orderMenus.put(menuId, new OrderItem(quantity, menuPrice));
-                    totalPrice += menuPrice * quantity;
-                }
-            }
-        }
-
-        OrderDelivery orderDelivery = new OrderDelivery();
-        orderDelivery.setCustomer(customer);
-        orderDelivery.setRestaurant(restaurant);
-        orderDelivery.setOrderMenus(orderMenus);
-        orderDelivery.setTotalPrice(totalPrice);
-        orderDelivery.setDate(new Date());
-        orderDelivery.setOrderStatus(OrderStatus.INPROGRESS);
-
-        orderService.save(orderDelivery);
+        orderService.orderDelivery(menuQuantities, restaurant, customer);
 
         return "customers/customerHome";
     }
--- src/main/java/MyEat/myeat/service/OrderService.java
+++ src/main/java/MyEat/myeat/service/OrderService.java
@@ -1,10 +1,14 @@
 package MyEat.myeat.service;
 
-import MyEat.myeat.domain.OrderDelivery;
+import MyEat.myeat.domain.*;
 import MyEat.myeat.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -12,8 +16,37 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MenuService menuService;
 
     public void save(OrderDelivery orderDelivery) {
         orderRepository.save(orderDelivery);
+    }
+
+    public void orderDelivery(Map<String, String> menuQuantities, Restaurant restaurant, Customer customer) {
+        Map<Long, OrderItem> orderMenus = new HashMap<>();
+        Long totalPrice = 0L;
+
+        for (Map.Entry<String, String> entry : menuQuantities.entrySet()) {
+            if (entry.getKey().startsWith("menuQuantities[")) {
+                Long menuId = Long.parseLong(entry.getKey().substring(15, entry.getKey().length() - 1));
+                int quantity = Integer.parseInt(entry.getValue());
+
+                if (quantity > 0) {
+                    Menu menu = menuService.findById(menuId); // 메뉴 정보를 DB에서 가져옴
+                    Long menuPrice = menu.getMenuPrice();
+                    orderMenus.put(menuId, new OrderItem(quantity, menuPrice));
+                    totalPrice += menuPrice * quantity;
+                }
+            }
+        }
+        OrderDelivery orderDelivery = new OrderDelivery();
+        orderDelivery.setCustomer(customer);
+        orderDelivery.setRestaurant(restaurant);
+        orderDelivery.setOrderMenus(orderMenus);
+        orderDelivery.setTotalPrice(totalPrice);
+        orderDelivery.setDate(new Date());
+        orderDelivery.setOrderStatus(OrderStatus.INPROGRESS);
+
+        save(orderDelivery);
     }
 }
